3225ad7c7bcafe29f3891e3480347ed2cb74acad
polimer-consumer <jenyhlevnoy@gmail.com>
2023-07-11 23:47:14 +0300

    changed to modules structure

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const path = require('path')
+import {add_user} from "./database/users.js";
+import express from "express";
+import path from "path"
+import bodyParser from "body-parser";
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
-const bodyParser = require('body-parser');
 
 app.set('view engine', 'html');
-
 app.use(express.static(path.join(__dirname, '')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', function (req, res) {
@@ -14,6 +18,8 @@ app.post('/registration', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
+
+    add_user(email, username, password);
 
     res.send('Registration successful!');
 });
