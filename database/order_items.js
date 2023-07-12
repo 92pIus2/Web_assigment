4b74dc5b3333b2daa0f417f01b81864676bbb4a9
fedorkukso <kuksofedor9@gmail.com>
2023-07-12 22:58:19 +0300

    Add GetOrder_item functions to DataBase

--- database/order_items.js
+++ database/order_items.js
@@ -84,3 +84,119 @@ export function delete_order_item(id) {
         });
     });
 }
+
+export function get_order_item_by_id(id) {
+    return new Promise((resolve, reject) => {
+        connection.connect((error) => {
+            if (error) {
+                console.error('Ошибка подключения к базе данных:', error);
+                reject(error);
+                return;
+            }
+            console.log('Подключено к базе данных MySQL');
+
+            // SQL-запрос для получения элемента заказа по ID
+            const selectQuery = 'SELECT * FROM order_items WHERE id = ?';
+
+            // Выполнение SQL-запроса для получения элемента заказа
+            connection.query(selectQuery, [id], (error, results) => {
+                if (error) {
+                    console.error('Ошибка при получении элемента заказа:', error);
+                    connection.end();
+                    reject(error);
+                    return;
+                }
+
+                // Возвращаем результаты
+                resolve(results[0]);
+
+                connection.end();
+            });
+        });
+    });
+}
+
+export function get_order_items_by_order_id(order_id) {
+    return new Promise((resolve, reject) => {
+        connection.connect((error) => {
+            if (error) {
+                console.error('Ошибка подключения к базе данных:', error);
+                reject(error);
+                return;
+            }
+            console.log('Подключено к базе данных MySQL');
+
+            // SQL-запрос для получения элементов заказа по ID заказа
+            const selectQuery = 'SELECT * FROM order_items WHERE order_id = ?';
+
+            // Выполнение SQL-запроса для получения элементов заказа
+            connection.query(selectQuery, [order_id], (error, results) => {
+                if (error) {
+                    console.error('Ошибка при получении элементов заказа:', error);
+                    connection.end();
+                    reject(error);
+                    return;
+                }
+
+                // Возвращаем результаты
+                resolve(results);
+
+                connection.end();
+            });
+        });
+    });
+}
+
+export function get_order_by_product_id(product_id) {
+    return new Promise((resolve, reject) => {
+        connection.connect((error) => {
+            if (error) {
+                console.error('Ошибка подключения к базе данных:', error);
+                reject(error);
+                return;
+            }
+            console.log('Подключено к базе данных MySQL');
+
+            // SQL-запрос для получения заказа по ID продукта
+            const selectQuery = `
+                SELECT orders.*
+                FROM orders
+                INNER JOIN order_items ON orders.id = order_items.order_id
+                WHERE order_items.product_id = ?
+            `;
+
+            // Выполнение SQL-запроса для получения заказа
+            connection.query(selectQuery, [product_id], (error, results) => {
+                if (error) {
+                    console.error('Ошибка при получении заказа:', error);
+                    connection.end();
+                    reject(error);
+                    return;
+                }
+
+                // Возвращаем результаты
+                resolve(results);
+
+                connection.end();
+            });
+        });
+    });
+}
+/*
+get_order_item_by_id(1).then((orderItem) => {
+    console.log(orderItem); // Log the retrieved order item by ID
+}).catch((error) => {
+    console.error(error); // Handle any errors
+});
+
+get_order_items_by_order_id(1).then((orderItems) => {
+    console.log(orderItems); // Log the retrieved order items by order ID
+}).catch((error) => {
+    console.error(error); // Handle any errors
+});
+
+get_order_by_product_id(1).then((orders) => {
+    console.log(orders); // Log the retrieved orders by product ID
+}).catch((error) => {
+    console.error(error); // Handle any errors
+});*/
