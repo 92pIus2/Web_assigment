322c756921d8bdd127bdf7c1b7d5624720600423
polimer-consumer <jenyhlevnoy@gmail.com>
2023-07-13 00:25:19 +0300

    Added dynamically loading content

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path"
 import bodyParser from "body-parser";
 import {fileURLToPath} from 'url';
+import {get_products_by_genre} from "./database/products.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,6 +15,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './first version of view.html'));
 });
+
+const items = [
+    {id: 1, name: 'Item 1', photo: 'template.png', description: 'Description 1', price: 10.99},
+    {id: 2, name: 'Item 2', photo: 'template.png', description: 'Description 2', price: 19.99},
+    {id: 3, name: 'Item 3', photo: 'template.png', description: 'Description 3', price: 19.99},
+    // Add more items as needed
+];
+
+// API endpoint to retrieve items
+app.get('/api/content', function (req, res) {
+    const items = get_products_by_genre('*').then((products) => {
+        console.log(products); // Log the retrieved products by genre
+    }).catch((error) => {
+        console.error(error); // Handle any errors
+    });
+    console.log(items);
+    res.json(items);
+});
+
+app.get('/content', (req, res) => {
+    res.sendFile('./index.html');
+});
+
 app.post('/registration', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
