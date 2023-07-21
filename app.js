d12f283992cd7889ce882f15f70a4bff082f5e83
fedorkukso <kuksofedor9@gmail.com>
2023-07-21 19:42:20 +0300

    Try to hosting at vercel + new port

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ app.post('/add_to_cart', async (req, res) => {
 const handler = (req, res) => app(req, res);
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
