3d2d4fdfe5402b8ad01132500b96253aae7f2e08
polimer-consumer <jenyhlevnoy@gmail.com>
2023-07-23 00:51:09 +0300

    Saving the branches from f*ck up

--- app.js
+++ app.js
@@ -105,6 +105,6 @@ app.post('/add_to_cart', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 });
